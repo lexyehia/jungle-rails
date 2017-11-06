6d5b66043200b6c23fc8f3ea558c1a2dfe63b853
Lex Yehia <lex.yehia@icloud.com>
2017-11-06 18:12:17 -0500

    Login functional

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(:email, params[:email]).try(:authenticate, params[:password])
+    user = User.find_by(email: params[:email]).authenticate(params[:password])
 
     if user
       cookies.encrypted[:user_session] = {
