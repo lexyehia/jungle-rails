3639693726f6b5f2703dab580e3d4f5a199759a5
Lex Yehia <lex.yehia@icloud.com>
2017-11-06 17:37:27 -0500

    Adding session management

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(:email, params[:email]).try(:authenticate, params[:password])
+
+    if user
+      cookies.encrypted[:user_session] = {
+        value: user.id,
+        expires: 5.days.from_now
+      }
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+end
