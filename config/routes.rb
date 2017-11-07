f84524bb3407b7ffc27d4595f5913f8e5a71c5ba
Lex Yehia <lex.yehia@icloud.com>
2017-11-07 14:55:04 -0500

    Implemented Review delete function

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show] do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:create, :destroy]
   end
-  
+
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
