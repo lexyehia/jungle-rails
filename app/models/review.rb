a3dec36d6ef973837b7a8014611efb49663f0f4b
Lex Yehia <lex.yehia@icloud.com>
2017-11-07 12:15:01 -0500

    Added validations to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ActiveRecord::Base
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
 end
