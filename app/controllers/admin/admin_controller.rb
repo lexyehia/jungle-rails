e050437ba97c54cdb6d9a402bfff009cfd7667da
Lex Yehia <lex.yehia@icloud.com>
2017-11-07 11:41:55 -0500

    Added http authentication with digest into Admin::AdminController, changed other admin controllers to inherit from it.

---
+++ app/controllers/admin/admin_controller.rb
@@ -0,0 +1,12 @@
+class Admin::AdminController < ApplicationController
+  USERS = { ENV["ADMIN_USERNAME"] => ENV["ADMIN_PASSWORD"] }
+
+  before_action :authenticate
+
+  private
+    def authenticate
+      authenticate_or_request_with_http_digest do |username|
+        USERS[username]
+      end
+    end
+end
