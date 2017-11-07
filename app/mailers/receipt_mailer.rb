fa81d74abeac207e4a53ce47b53b32a8e395ed86
Lex Yehia <lex.yehia@icloud.com>
2017-11-07 09:40:47 -0500

    Done basic email for order receipts

--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -1,2 +1,7 @@
 class ReceiptMailer < ApplicationMailer
+
+  def order_receipt_email(user)
+    @order = order
+    mail(to: @order.email, subject: "Your Receipt for Order ##{@order.id}")
+  end
 end
