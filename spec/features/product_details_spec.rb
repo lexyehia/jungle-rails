255b7a012560c99f94035c002864569347cc7e23
Lex Yehia <lex.yehia@icloud.com>
2017-11-14 15:45:04 -0500

    Added product details feature spec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    all('article.product header a').first.click
+    save_screenshot
+
+    expect(page).to have_css 'section.products-show', count: 1
+  end
+end
